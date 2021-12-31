740802402dab5ef269323e880ff8fc9df0b4c4e4
Sebastian Hellum <sebhellum@gmail.com>
2021-12-31 09:21:42 +0100

    Loop through testing posts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import Head from "next/head";
 
+const posts = [
+  {
+    title: "React Testing",
+    excerpt: "This is a test",
+  },
+  {
+    title: "React Testing 2",
+    excerpt: "This is a test 2",
+  },
+];
+
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -7,6 +18,14 @@ export default function Home() {
         <title>Turnusgutta</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        {posts.map((post, index) => (
+          <div>
+            {post.title}
+            {post.excerpt}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
