348078456684b264b4cd51fa4d8c3702c4c8c33d
Sebastian Hellum <sebhellum@gmail.com>
2021-12-31 10:56:16 +0100

    Fetching data from database

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,17 @@
-import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {
-    title: "React Testing",
-    excerpt: "This is a test",
-  },
-  {
-    title: "React Testing 2",
-    excerpt: "This is a test 2",
-  },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Turnusgutta</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div classname="lg:sticky realtive top-8">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
@@ -34,4 +19,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+// Fetching data
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
